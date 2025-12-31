45f769702afc05bfd0197c6aa1f8ff41a60380f8
cpaton <cpaton@github.com>
2025-12-31 11:32:56 +0000

    feat: include hidden files in backups

--- scripts/backup.ps1
+++ scripts/backup.ps1
@@ -23,63 +23,82 @@ param(
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 
-$foldersToBackup = @(
-    "/opt/homeassistant/"
-    "/opt/mosquitto/"
-    # "/opt/portainer/"
-    "/opt/zigbee2mqtt/"
-)
+try {
+    $foldersToBackup = @(
+        "/opt/homeassistant/"
+        "/opt/mosquitto/"
+        # "/opt/portainer/"
+        "/opt/zigbee2mqtt/"
+    )
 
-$backupFilenamePrefix = "home-automation-backup-"
-$backupTime = [DateTime]::UtcNow
-$dailyBackupDateFormat = "yyyy-MM-dd"
-$monthlyBackupDateFormat = "yyyy-MMM"
-$dailyBackupFileName = "$($backupFilenamePrefix)$($backupTime.ToString($dailyBackupDateFormat)).zip"
-$previousMonthBackupFileName = "$($backupFilenamePrefix)$($backupTime.AddMonths(-1).ToString($monthlyBackupDateFormat)).zip"
-$dailyBackupPath = Join-Path $Target $dailyBackupFileName
-$previousMonthBackupPath = Join-Path $Target $previousMonthBackupFileName
+    $backupFilenamePrefix = "home-automation-backup-"
+    $backupTime = [DateTime]::UtcNow
+    $dailyBackupDateFormat = "yyyy-MM-dd"
+    $monthlyBackupDateFormat = "yyyy-MMM"
+    $dailyBackupFileName = "$($backupFilenamePrefix)$($backupTime.ToString($dailyBackupDateFormat)).tgz"
+    $previousMonthBackupFileName = "$($backupFilenamePrefix)$($backupTime.AddMonths(-1).ToString($monthlyBackupDateFormat)).tgz"
+    $dailyBackupPath = Join-Path $Target $dailyBackupFileName
+    $previousMonthBackupPath = Join-Path $Target $previousMonthBackupFileName
 
-Write-Host "Creating new backup $($dailyBackupPath)" -ForegroundColor Blue
-Compress-Archive `
-    -Path $foldersToBackup `
-    -DestinationPath $dailyBackupPath `
-    -Force `
-    -Verbose:$false
-chown $User $dailyBackupPath
-chgrp root $dailyBackupPath
-chmod g+w $dailyBackupPath
+    $tarArguments = @(
+        "tar"
+        "--create"
+        "--gzip"
+        "--file=$($dailyBackupPath)"
+        "--recursion"
+        "--verbose"
+    )
+    foreach ($folder in $foldersToBackup) {
+        $folderParent = Split-Path -Path $folder -Parent
+        $folderName = Split-Path -Path $folder -Leaf
+        $tarArguments += "--directory=$folderParent"
+        $tarArguments += "$folderName"
+    }
+    $tarCommand = $tarArguments -join " "
 
-if (-not (Test-Path $previousMonthBackupFileName)) {
-    Write-Host "Storing monthly backup $($previousMonthBackupPath)"
-    Copy-Item -Path $dailyBackupPath -Destination $previousMonthBackupPath
-    chown $User $previousMonthBackupPath
-    chgrp root $previousMonthBackupPath
+    Write-Host "Creating new backup $($dailyBackupPath)" -ForegroundColor Blue
+    Write-Host "Executing: $tarCommand" -ForegroundColor Gray
+    Invoke-Expression $tarCommand
+    chown $User $dailyBackupPath
+    chgrp root $dailyBackupPath
+    chmod g+w $dailyBackupPath
+
+    if (-not (Test-Path $previousMonthBackupPath)) {
+        Write-Host "Storing monthly backup $($previousMonthBackupPath)"
+        Copy-Item -Path $dailyBackupPath -Destination $previousMonthBackupPath
+        chown $User $previousMonthBackupPath
+        chgrp root $previousMonthBackupPath
+    }
+
+    $earliestBackupToKeep = [datetime]::Today.AddDays(-1 * $DaysToKeep)
+    Write-Host "Processing existing backups, keeping up until $($earliestBackupToKeep.ToString($dailyBackupDateFormat))" -ForegroundColor Blue
+    $backupFiles = Get-ChildItem -Path $Target -File -Recurse:$false | Where-Object { $_.Extension -in @(".zip", ".tgz") }
+    foreach ($backupFile in $backupFiles) {
+        Write-Host $backupFile.FullName -ForegroundColor Gray
+
+        # Daily backup?
+        if ($backupFile.Name -match "$([System.Text.RegularExpressions.Regex]::Escape($backupFilenamePrefix))(?<date>\d{4}-\d{2}-\d{2})\.(zip|tgz)$") {
+            $backupDate = [DateTime]::ParseExact($Matches["date"], $dailyBackupDateFormat, [System.Globalization.CultureInfo]::InvariantCulture)
+            if ($backupDate -ge $earliestBackupToKeep) {
+                Write-Verbose "Keeping backup as within time window"
+            }
+            else {
+                Write-Host "Removing old backup $($backupFile.Name)" -ForegroundColor Yellow
+                Remove-Item -Path $backupFile.FullName
+            }
+
+            continue
+        }
+
+        if ($backupFile.Name -match "$([System.Text.RegularExpressions.Regex]::Escape($backupFilenamePrefix))(?<date>\d{4}-\w{3})\.(zip|tgz)$") {
+            Write-Verbose "Ignoring monthly backup"
+            continue
+        }
+
+        Write-Warning "Unable to parse date from backup file $($backupFile.Name)"
+    }
 }
-
-$earliestBackupToKeep = [datetime]::Today.AddDays(-1 * $DaysToKeep)
-Write-Host "Processing existing backups, keeping up until $($earliestBackupToKeep.ToString($dailyBackupDateFormat))" -ForegroundColor Blue
-$backupFiles = Get-ChildItem -Path $Target -Filter "*.zip"
-foreach ($backupFile in $backupFiles) {
-    Write-Host $backupFile.FullName -ForegroundColor Gray
-
-    # Daily backup?
-    if ($backupFile.Name -match "$([System.Text.RegularExpressions.Regex]::Escape($backupFilenamePrefix))(?<date>\d{4}-\d{2}-\d{2})\.zip") {
-        $backupDate = [DateTime]::ParseExact($Matches["date"], $dailyBackupDateFormat, [System.Globalization.CultureInfo]::InvariantCulture)
-        if ($backupDate -ge $earliestBackupToKeep) {
-            Write-Verbose "Keeping backup as within time window"
-        }
-        else {
-            Write-Host "Removing old backup $($backupFile.Name)" -ForegroundColor Yellow
-            Remove-Item -Path $backupFile.FullName
-        }
-
-        continue
-    }
-
-    if ($backupFile.Name -match "$([System.Text.RegularExpressions.Regex]::Escape($backupFilenamePrefix))(?<date>\d{4}-\w{3})\.zip") {
-        Write-Verbose "Ignoring monthly backup"
-        continue
-    }
-
-    Write-Warning "Unable to parse date from backup file $($backupFile.Name)"
+catch {
+    Get-Error
+    throw
 }
